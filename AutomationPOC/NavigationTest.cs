ac0081ccfd95047c0b9da8dbaa0f262afd56e546
Usuario <Usuario@DESKTOP-I97FENN>
2024-05-25 03:27:58 -0400

    Add project files.

---
+++ AutomationPOC/NavigationTest.cs
@@ -0,0 +1,16 @@
+namespace AutomationPOC
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
